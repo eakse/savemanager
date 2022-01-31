ac9231596edf9c7e2822f32c092a1b3a39465dcb
eakse <edo@akse.cloud>
2022-02-01 00:16:28 +0100

    01

--- main.py
+++ main.py
@@ -13,10 +13,8 @@ import inspect
 def dump_args(func):
     """
     Decorator to print function call details.
-
     This includes parameters names and effective values.
     """
-
     def wrapper(*args, **kwargs):
         func_args = inspect.signature(func).bind(*args, **kwargs).arguments
         func_args_str = ", ".join(map("{0[0]} = {0[1]!r}".format, func_args.items()))
