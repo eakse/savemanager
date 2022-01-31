412fe434543e59cf390da13ed5c1ad7678d1ac4e
eakse <edo@akse.cloud>
2022-02-01 00:15:30 +0100

    01

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import __future__
+from ast import dump
+from msilib.schema import Directory
 import PySimpleGUI as sg
 import os
 import py7zr
 import json
 from datetime import datetime
+import time
+import inspect
+
+
+def dump_args(func):
+    """
+    Decorator to print function call details.
+
+    This includes parameters names and effective values.
+    """
+
+    def wrapper(*args, **kwargs):
+        func_args = inspect.signature(func).bind(*args, **kwargs).arguments
+        func_args_str = ", ".join(map("{0[0]} = {0[1]!r}".format, func_args.items()))
+        print(f"{func.__module__}.{func.__qualname__} ( {func_args_str} )")
+        return func(*args, **kwargs)
+
+    return wrapper
 
 
 def set_layout():
@@ -61,41 +81,52 @@ def set_layout():
 
 
 # sg.theme("Dark")  # Add a touch of color
-sg.ChangeLookAndFeel("Dark")  # Add a touch of color
+settings = {}
+sg.ChangeLookAndFeel("Black")  # Add a touch of color
 settings_filename = "./savemanager_settings.json"
 window = sg.Window("Save Manager", set_layout())
 window.Font = ("Consolas", 8)
 window.finalize()
 
 
+# @dump_args
 def now():
     return datetime.now().strftime("%H:%M:%S")
 
 
+# @dump_args
 def log(text):
-    window["-LOG-"].print(f"{now()} | {text}")
+    lines = text.split("\n")
+    window["-LOG-"].print(f"{now()} | {lines[0]}")
+    for line in lines[1:]:
+        window["-LOG-"].print(f"         | {line}")
     window["-LOG-"].update()
+    window.refresh()
 
 
+# @dump_args
 def load_settings(filename=settings_filename):
+    global settings
     log(f"Loading settings from {filename}")
     with open(filename) as infile:
         settings = json.load(infile)
-    log(f"SETTINGS = \n{json.dumps(settings, indent=4)}")
-    return settings
+    # log(f"SETTINGS = \n{json.dumps(settings, indent=4)}")
 
 
-def save_settings(settings, filename=settings_filename):
+# @dump_args
+def save_settings(filename=settings_filename):
     log(f"Saving settings to {filename}")
     with open(filename, "w") as outfile:
         json.dump(settings, outfile, indent=4)
-    log(f"SETTINGS = \n{json.dumps(settings, indent=4)}")
+    # log(f"SETTINGS = \n{json.dumps(settings, indent=4)}")
 
 
+# @dump_args
 def settings_init():
     if os.path.exists(settings_filename):
-        settings = load_settings()
+        load_settings()
     else:
+        global settings
         log("No settings file found, creating default settings...")
         settings = {
             "FILE_EXT": ".7z",
@@ -104,61 +135,83 @@ def settings_init():
             "SOURCE_FOLDER": "./",
             "DEST_FOLDER": "./",
         }
-        save_settings(settings)
-    return settings
+        save_settings()
 
 
-def create_backup(settings):
-    log("\nStarted backup...")
-    with py7zr.SevenZipFile(add_new_backup(settings), "w", ) as outfile:
-        outfile.writeall(settings['SOURCE_FOLDER'])
+# @dump_args
+def create_backup(filename, extralog=""):
+    logstr = "Starting backup, application might freeze a bit..."
+    if extralog != "":
+        logstr = extralog + "\n" + logstr
+    log(logstr)
+    directory = settings["SOURCE_FOLDER"]
+    with py7zr.SevenZipFile(filename, "w") as outfile:
+        rootdir = os.path.basename(directory)
+        for dirpath, dirnames, filenames in os.walk(directory):
+            for filename in filenames:
+                filepath = os.path.join(dirpath, filename)
+                parentpath = os.path.relpath(filepath, directory)
+                arcname = os.path.join(rootdir, parentpath)
+                # log(f"Adding: {filepath}")
+                outfile.write(filepath, arcname)
+    get_current_backups()
 
 
-def get_files_from_path(settings) -> list:
-    result = []
-    for subdir, dirs, files in os.walk(settings["DEST_FOLDER"]):
-        for fname in files:
-            filepath = f"{subdir}{os.sep}{fname}"
-            if fname.lower().endswith(settings["FILE_EXT"].lower()) and fname.startswith(settings["FILE_NAME"]):
-                result.append(filepath)
-    if len(result) > 0:
-        return result.sort()
-    else:
-        return [f"{settings['DEST_FOLDER']}{os.sep}{settings['FILE_NAME']}0{settings['FILE_EXT']}"]
+# @dump_args
+def add_new_backup():
+    newbackupname = increase(get_current_highest())
+    start = time.time()
+    create_backup(
+        f"{settings['DEST_FOLDER']}{os.sep}{newbackupname}",
+        extralog=f"Creating new backup with filename:\n  {newbackupname}",
+    )
+    log(f"Backup done.\nTime elapsed: {time.time()-start:.2f} seconds")
 
 
-def inc_filename(filename: str) -> str:
-    # using my own answer from SO::
-    ## https://stackoverflow.com/a/69054013/9267296
-    if "." in filename:
-        # set extension
-        extension = f""".{filename.split(".")[-1]}"""
-        # remove extension from filename
-        filename = ".".join(filename.split(".")[:-1])
-    else:
-        # set extension to empty if not included
-        extension = ""
-    try:
-        # try to set the number
-        # it will throw a ValueError if it doesn't have _1
-        number = int(filename.split("_")[-1]) + 1
-        newfilename = "_".join(filename.split("_")[:-1])
-    except ValueError:
-        # catch the ValueError and set the number to 1
-        number = 1
-        newfilename = "_".join(filename.split("_"))
-    return f"{newfilename}_{number}{extension}"
+# @dump_args
+def get_current_backups() -> list:
+    result = [
+        filename
+        for filename in next(os.walk(settings["DEST_FOLDER"]), (None, None, []))[2]
+        if (
+            filename.startswith(settings["FILE_NAME"])
+            and filename.lower().endswith(settings["FILE_EXT"].lower())
+        )
+    ]
+    window["-BACKUP LIST-"].update(result)
+    window.refresh()
+    return result
 
 
-def add_new_backup(settings):
-    filelist = get_files_from_path(settings)
-    return inc_filename(filelist[-1])
+# @dump_args
+def get_number(filename: str) -> int:
+    return filename.split(settings["FILE_NAME"])[1].split(settings["FILE_EXT"])[0]
 
 
+# @dump_args
+def increase(filename: str) -> str:
+    nr = 1 + int(
+        filename.split(settings["FILE_NAME"])[1].split(settings["FILE_EXT"])[0]
+    )
+    return f"{filename.split(settings['FILE_NAME'])[0]}{settings['FILE_NAME']}{nr}{settings['FILE_EXT']}"
+
+
+# @dump_args
+def get_current_highest() -> str:
+    highest = f"{settings['FILE_NAME']}-1{settings['FILE_EXT']}"
+    for filename in get_current_backups():
+        if get_number(filename) > get_number(highest):
+            highest = filename
+    return highest
+
+
+# @dump_args
 def main():
-    settings = settings_init()
+    global settings
+    settings_init()
     window["-SOURCE-"].update(settings["SOURCE_FOLDER"])
     window["-DEST-"].update(settings["DEST_FOLDER"])
+    get_current_backups()
 
     while True:
         event, values = window.read()
@@ -167,7 +220,7 @@ def main():
 
         elif event == "-BACKUP-":
             # create BACKUP
-            create_backup(settings)
+            add_new_backup()
 
         elif event == "-RESTORE-":
             # restore previous backupp
@@ -182,12 +235,12 @@ def main():
         elif event == "-SOURCE-":
             # set SOURCE folder
             settings["SOURCE_FOLDER"] = values["-SOURCE-"]
-            save_settings(settings)
+            save_settings()
 
         elif event == "-DEST-":
             # set DEST folder
             settings["DEST_FOLDER"] = values["-DEST-"]
-            save_settings(settings)
+            save_settings()
 
         elif event == "-BACKUP LIST-":  # A file was chosen from the listbox
             try:
